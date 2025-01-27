91833d512eb96a3b98c3b6302898d9fe2e0ad057
TJayOliver <tjoliver1@yahoo.com>
2025-01-27 23:39:25 +0000

    new homepage

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,123 +1,116 @@
-/* eslint-disable react/prop-types */
-import { useEffect, useState, useRef } from "react";
-import { CiSearch } from "react-icons/ci";
-import voting1 from "./assets/v1.jpg";
-import voting2 from "./assets/v2.jpg";
-import voting3 from "./assets/v3.jpg";
-import voting4 from "./assets/v4.jpg";
-import { Header } from "./components/Header.jsx"
+import { useState } from "react";
+import phone from "./assets/iphone.png";
 import { Footer } from "./components/Footer";
-
-const HomepageSections = ({
-  buttonName,
-  title,
-  color,
-  buttonColor,
-  buttonTextColor,
-  buttonHoverColor,
-  to,
-}) => {
-  return (
-    <div
-      className={`flex flex-col justify-around items-center border-[1px] border-gray-300 ${color} p-2 rounded-lg w-72 h-48`}
-    >
-      <p className="w-24 font-medium text-center">{title}</p>
-      <a
-        href={to}
-        className={`${buttonColor} text-center p-3 rounded-lg w-2/4 font-medium hover:duration-100 hover:ease-in hover:${buttonHoverColor} ${buttonTextColor}`}
-      >
-        {buttonName}
-      </a>
-    </div>
-  );
-};
+import { IoMdTrendingUp } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Homepage = () => {
-  // const [hamburger, setHamburger] = useState(false);
-  const [searchIcon, setSearchIcon] = useState(true);
-  const hideSearchIcon = () => {
-    setSearchIcon(false);
+  const [nextLatest, setNextLatest] = useState(false);
+  let classNext = `absolute left-56 duration-100 ease-out  `;
+  const swipeNext = () => {
+    setNextLatest((prev) => !prev);
   };
-  const displaySearchIcon = () => {
-    setSearchIcon(true);
-  };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const images = [voting1, voting2, voting3, voting4];
-  const [slide, setSlide] = useState(images[0]);
-  const countRef = useRef(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      countRef.current = (countRef.current + 1) % images.length;
-      setSlide(images[countRef.current]);
-    }, 50000);
-
-    return () => clearInterval(interval);
-  }, [images]);
-
-  return (
-    <>
-      <Header />
-      {/* banner */}
-      <aside className="top-0 left-0 -z-50 fixed w-screen h-screen overflow-hidden">
-        <img src={voting1} className="absolute inset-0 w-full h-full object-cover" />
-      </aside>
-
-      <main className="flex flex-col md:gap-8 m-auto mb-14 max-w-7xl">
-        {/* heading */}
-        <section className="relative items-center place-content-center gap-8 grid p-4 text-center Outfit">
-          <div className="drop-shadow-sm text-white">
-            <p className="font-medium text-xl md:text-3xl">Welcome to</p>
-            <div className="flex flex-col items-center text-5xl md:text-8xl Pacifico">
-              <p>The New Way</p>
-              <p>of Voting</p>
+  const LatestBox = () => {
+    return (
+      <div className="bg-white/80 p-3 relative text-black/80 h-56 w-64 rounded-xl font-medium flex flex-col justify-between">
+        <div className="flex justify-between items-center">
+          <div className="bg-gray-200 h-10 w-10 flex items-center justify-center rounded-full">
+            <IoMdTrendingUp />
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <div
+              onClick={swipeNext}
+              role="button"
+              className="bg-[#54ea54] h-6 w-6 flex items-center justify-center rounded-full"
+            >
+              <IoIosArrowForward />
+            </div>
+            <div className="flex flex-col gap-1">
+              <div className="rounded-full h-1 w-1 bg-black"></div>
+              <div className="rounded-full h-1 w-1 bg-black/50"></div>
+              <div className="rounded-full h-1 w-1 bg-black/50"></div>
             </div>
           </div>
-        </section>
+        </div>
+        <p>Latest Polls</p>
+        <hr></hr>
+        <div className="relative">
+          <div className="absolute hidden">
+            <p className="text-xl">UENR NUGS AWARDS</p>
+            <small className=" line-clamp-3">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Voluptatibus aliquid, quibusdam molestiae repudiandae labore vel
+              tenetur, ad a numquam sapiente commodi asperiores cumque eos
+              repellendus excepturi quos error in totam.
+            </small>
+          </div>
+          <div className="absolute hidden">
+            <p className="text-xl">UENR NUGS AWARDS</p>
+            <small className=" line-clamp-3">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Voluptatibus aliquid, quibusdam molestiae repudiandae labore vel
+              tenetur, ad a numquam sapiente commodi asperiores cumque eos
+              repellendus excepturi quos error in totam.
+            </small>
+          </div>
+          <div className="">
+            <p className="text-xl">UENR NUGS AWARDS</p>
+            <small className=" line-clamp-3">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Voluptatibus aliquid, quibusdam molestiae repudiandae labore vel
+              tenetur, ad a numquam sapiente commodi asperiores cumque eos
+              repellendus excepturi quos error in totam.
+            </small>
+          </div>
+        </div>
+        <hr></hr>
+      </div>
+    );
+  };
+  return (
+    <>
+      <header className="h-14 font-medium text-white flex justify-center md:justify-evenly p-4 sticky top-0 z-10 mb-8">
+        <div className="flex space-x-4 justify-between items-center bg-white/30 backdrop-blur shadow-lg rounded-xl p-6 w-auto md:w-[40rem]">
+          <h1 className=" font-mineBold">Votely</h1>
+          <div className=" hidden  md:flex space-x-14">
+            <p>Home</p>
+            <p>Features</p>
+            <p>Event</p>
+            <p>About</p>
+          </div>
+          <button className="bg-[#54ea54] text-black text-sm flex items-center h-8 p-2 rounded-xl ">
+            Sign in
+          </button>
+        </div>
+      </header>
 
-        {/* search form */}
-        <section className="flex p-2 w-full">
-          <form className="relative flex items-center w-full">
-            <CiSearch className={searchIcon ? "right-4 absolute text-3xl " : "hidden"} />
-            <input
-              type="search"
-              placeholder="Enter Event or Organizers Name"
-              onFocus={hideSearchIcon}
-              onBlur={displaySearchIcon}
-              className="border-[0.5px] border-gray-400 bg-gray-50 focus:bg-gray-100 px-2 rounded-md w-full h-10 outline-none"
-            />
-          </form>
+      <main
+        className={`text-white flex flex-col items-center justify-center p-2 md:p-8 min-h-[calc(100vh-232px)] relative bg-center bg-cover space-y-8`}
+      >
+        <h2 className="text-3xl md:text-5xl Bebas text-center">
+          "Where Every Vote <br></br> meets Technology"
+        </h2>
+        <section className="flex space-x-4">
+          <button className="rounded-full text-black font-medium items-center text-sm p-2 bg-[#54ea54]">
+            Explore Polls
+          </button>
+          <button className="rounded-full text-black font-medium items-center text-sm p-2 bg-white ">
+            Create a Poll
+          </button>
         </section>
-
-        {/* menus */}
-        <section className="flex md:flex md:flex-row flex-col flex-wrap justify-between md:justify-around items-center gap-4 p-2">
-          <HomepageSections
-            buttonName={"Sign Up"}
-            title={"Sign Up to Create a Poll"}
-            color={"bg-[#E7D4B5]"}
-            buttonColor={"bg-[#f1eaea]"}
-            buttonHoverColor={"bg-gray-100"}
-            to={"/user/registration"}
+        <section className="flex justify-between items-center">
+          <div className="flex flex-col gap-2">
+            <LatestBox />
+            <div className="bg-gradient-to-r from-sky-400 to-cyan-300 shadow-md h-56 w-64 rounded-xl "></div>
+          </div>
+          <img
+            src={phone}
+            className="object-cover bg-center h-[50rem] overflow-x-hidden"
           />
-          <HomepageSections
-            buttonName={"Explore"}
-            title={"Explore Recent Competitions"}
-            color={"bg-gray-200"}
-            buttonColor={"bg-[#272829]"}
-            buttonHoverColor={"bg-[#272819]"}
-            buttonTextColor={"text-white"}
-            to={"/event"}
-          />
-          <HomepageSections
-            buttonName={"Sign In"}
-            title={"Log In and enjoy the experience"}
-            color={"bg-[#B6C7AA]"}
-            buttonColor={"bg-[#f1eaea]"}
-            buttonHoverColor={"bg-gray-100"}
-            to={"/user/signin"}
-          />
+          <div className="bg-white h-96 w-64 rounded-xl "></div>
         </section>
       </main>
+
       <Footer />
     </>
   );
